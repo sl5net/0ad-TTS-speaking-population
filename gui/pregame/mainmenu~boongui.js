ba94c2183483243fecb18599b28114aa5ae23013
sl5net <sl5softwarelab@gmail.com>
2022-11-22 02:26:24 +0100

    fix wrong variable was used

--- gui/pregame/mainmenu~boongui.js
+++ gui/pregame/mainmenu~boongui.js
@@ -123,14 +123,15 @@ check_tts_Settings();
 
 function saveThisModProfile(nr){
 	const modsFromUserCfg_const = Engine.ConfigDB_GetValue("user", "mod.enabledmods");
-
 	const modProfile = Engine.ConfigDB_GetValue("user", "modProfile.p" + nr);
-	let clean = modProfile.replaceAll(/[^\w\d_]+/g,' ');
-	clean = modProfile.replaceAll(/\b(mod public)\b\s*/g,''); // mod public is default. boring to save it
 	if(!modProfile){
+		let clean = modsFromUserCfg_const.replaceAll(/[^\w\d_]+/g,' ');
+		clean = modsFromUserCfg_const.replaceAll(/\b(mod public)\b\s*/g,''); // mod public is default. boring to save it
 		Engine.ConfigDB_WriteValueToFile("user", "modProfile.p" + nr, clean, "config/user.cfg"); // fill it if its empty
 	}else{
-		if(clean != modProfile){
+		let clean = modProfile.replaceAll(/[^\w\d_]+/g,' ');
+		clean = modProfile.replaceAll(/\b(mod public)\b\s*/g,''); // mod public is default. boring to save it
+			if(clean != modProfile){
 			Engine.ConfigDB_WriteValueToFile("user", "modProfile.p" + nr, clean, "config/user.cfg"); // 
 			warn();('modProfile.p1 saved with =' + clean + '=');
 		}
